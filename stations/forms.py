0dc2581741118c356d667e8584c2ce6266ec46b6
Kiarash <shahedikiarash@gmail.com>
2024-06-16 19:04:07 +0330

    add pdf generating and some calculations

--- stations/forms.py
+++ stations/forms.py
@@ -7,29 +7,67 @@ class FuelStationForm(forms.ModelForm):
         model = FuelStation
         fields = ['name', 'gasoline_tanks', 'gasoline_nozzles', 'gas_tanks', 'gas_nozzles', 'control_period', 'product_type',
                   'gasoline_initial_stock', 'gasoline_received', 'gasoline_electronic_sales',
-                  'gas_initial_stock', 'gas_received', 'gas_tank1_stock', 'gas_tank2_stock', 'diesel_electronic_sales']
+                  'gas_initial_stock', 'gas_received', 'diesel_electronic_sales']
+        labels = {
+            'name': 'نام',
+            'gasoline_tanks': 'تعداد مخازن بنزین',
+            'gasoline_nozzles': 'تعداد نازل‌های بنزین',
+            'gas_tanks': 'تعداد مخازن گاز',
+            'gas_nozzles': 'تعداد نازل‌های گاز',
+            'control_period': 'دوره کنترل',
+            'product_type': 'نوع محصول',
+            'gasoline_initial_stock': 'موجودی اولیه بنزین',
+            'gasoline_received': 'بنزین دریافتی',
+            'gasoline_electronic_sales': 'فروش الکترونیکی بنزین',
+            'gas_initial_stock': 'موجودی اولیه گاز',
+            'gas_received': 'گاز دریافتی',
+            'diesel_electronic_sales': 'فروش الکترونیکی گازوئیل',
+        }
 
 class GasolineNozzleForm(forms.ModelForm):
     class Meta:
         model = GasolineNozzle
         fields = ['nozzle_number', 'totalizer_start', 'totalizer_end']
-
+        labels = {
+            'nozzle_number': 'شماره نازل',
+            'totalizer_start': 'شروع توتالایزر',
+            'totalizer_end': 'پایان توتالایزر',
+        }
+        
+        
+        
 class GasNozzleForm(forms.ModelForm):
     class Meta:
         model = GasNozzle
         fields = ['nozzle_number', 'totalizer_start', 'totalizer_end']
-
+        labels = {
+            'nozzle_number': 'شماره نازل',
+            'totalizer_start': 'شروع توتالایزر',
+            'totalizer_end': 'پایان توتالایزر',
+        }
+        
+        
+        
 
 class GasolineTankForm(forms.ModelForm):
     class Meta:
         model = GasolineTank
         fields = ['tank_number', 'final_stock']
-
+        labels = {
+            'tank_number': 'شماره مخزن',
+            'final_stock': 'موجودی مخزن',
+        }
+        
+        
+        
 class GasTankForm(forms.ModelForm):
     class Meta:
         model = GasTank
         fields = ['tank_number', 'final_stock']
-
+        labels = {
+            'tank_number': 'شماره مخزن',
+            'final_stock': 'موجودی مخزن',
+        }
 
 
 
